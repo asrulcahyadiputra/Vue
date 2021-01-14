8194085ddcc0237934c8cf322bbee33b04811c6e
Asrul Cahyadi Putra <asrulcahyadiputra99@gmail.com>
2021-01-15 00:09:01 +0700

    Conditional Directive

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,8 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello World !'
+        show: true,
+        message: 'Hello World !',
+        message2: 'Hello Vue !'
     }
 });
